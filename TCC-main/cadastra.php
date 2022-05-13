e8c3b38a094a7dd4f20579e6d6eadab8ca06b1d6
Nicolas Palma <61471877+nnicolaspalma@users.noreply.github.com>
2022-05-13 16:57:32 -0300

    Tcc_Atualizado

---
+++ TCC-main/cadastra.php
@@ -0,0 +1,23 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <title>cadastro
+        </title>
+        <link rel="stylesheet"  href="css/estilo.css">
+    </head>
+
+        <body>
+            <div id="corpo-form-cad">
+            <h1> Cadastrar </h1>
+            <form>
+            <input type="nome" placeholder="Digite seu nome">
+                <input type="email" placeholder="Digite seu email">
+                <input type="telefone" placeholder="Seu telefone">
+                <input type="passaword" placeholder="Digite uma senha">
+                <input type="passaword" placeholder="Confirma senha">
+                <input type="submit" value="Cadastrar">
+               
+            </div>
+        </body>
+    
+</html>
---
+++ TCC-main/index.php
@@ -0,0 +1,23 @@
+
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Login
+        </title>
+        <link rel="stylesheet"  href="css/estilo.css">
+    </head>
+
+        <body>
+            <div id="corpo-form">
+            <h1> Login </h1>
+            <form>
+                
+                <input type="email" placeholder="digite seu email">
+                <input type="passaword" placeholder="digite sua senha">
+                <input type="submit" value="Acessar">
+                <a href="cadastra.php">Ainda nao é inscrito?<strong>cadastrar</a>
+            </form>
+            </div>
+        </body>
+    
+</html>
